5408c95cb4c936b1d480ad72b7ae1cd1eea446f3
Anand Patel <anandkumarpatel@gmail.com>
2024-02-15 17:22:02 -0800

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function getBody(path: string) {
   if (path.includes('bedtime')) {
     return <>body</>
@@ -10,7 +5,6 @@ function getBody(path: string) {
   return <>other</>
 }
 function App() {
-  const [count, setCount] = useState(0)
   const path = window.location.pathname
   const body = getBody(path)
   return <>{body}</>
